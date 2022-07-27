389b2ab5e8975e96b9d80e86ccd4788096bf56e9
Zeamanual Feleke <91523382+zeamanual@users.noreply.github.com>
2022-07-27 16:06:09 +0300

    Create Counting Sort 1.py

---
+++ Counting Sort 1/Counting Sort 1.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    array_size = len(arr)
+    frequency_arr = []
+    for i in range(100):
+        frequency_arr.append(0)
+    for num in arr:
+        frequency_arr[num]+=1
+    
+    return frequency_arr
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
