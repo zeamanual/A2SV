7a2e3b6ad37e2a39d72282c0b2e31449711d9ac9
Zeamanual Feleke <91523382+zeamanual@users.noreply.github.com>
2023-04-22 19:49:38 +0300

    Update 1649. Create Sorted Array through Instructions.py

--- 1649. Create Sorted Array through Instructions/1649. Create Sorted Array through Instructions.py
+++ 1649. Create Sorted Array through Instructions/1649. Create Sorted Array through Instructions.py
@@ -4,13 +4,11 @@ class Solution:
 
         cost = 0
         nums = SortedList()
-        freq = dict()
 
         for num in instructions:
             option1 =  nums.bisect_left(num)
             option2 = len(nums) - nums.bisect_right(num)
             cost = (cost + min(option1, option2))%(10**9+7)
             nums.add(num)
-            freq[num] = freq.get(num,0)+1
 
         return cost
