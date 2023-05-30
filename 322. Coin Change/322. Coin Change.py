486b47e5e926730ecfa381dac194d8104f2db51c
Zeamanual Feleke <91523382+zeamanual@users.noreply.github.com>
2023-05-30 17:28:42 +0300

    Create 322. Coin Change.py

---
+++ 322. Coin Change/322. Coin Change.py
@@ -0,0 +1,14 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0 and dp[i - coin] != float('inf'):
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        
+        if dp[amount] == float('inf'):
+            return -1
+        else:
+            return dp[amount]
