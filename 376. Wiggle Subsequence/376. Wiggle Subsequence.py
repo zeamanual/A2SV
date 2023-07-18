a5b8b003f8163f36e7fba65d5515e74ebc6fd622
Zeamanual Feleke <91523382+zeamanual@users.noreply.github.com>
2023-07-18 21:10:01 +0300

    Update 376. Wiggle Subsequence.py

--- 376. Wiggle Subsequence/376. Wiggle Subsequence.py
+++ 376. Wiggle Subsequence/376. Wiggle Subsequence.py
@@ -8,12 +8,12 @@ class Solution:
         last_sign = 0
 
         for i in range(1,len(nums)):
-            if(last_sign ==0 ):
-                last_sign = 1 if nums[i]-nums[i-1] else -1
+            if(last_sign ==0 and nums[i]-nums[i-1]!=0 ):
+                last_sign = 1 if nums[i]-nums[i-1] < 0 else -1
 
             if((nums[i]-nums[i-1] > 0 and last_sign==-1 ) or (nums[i]-nums[i-1] <  0 and last_sign==1)):
                 max_length+=1
-                last_sign = 1 if nums[i]-nums[i-1] else -1
+                last_sign = 1 if nums[i]-nums[i-1] > 0 else -1
 
         
         return max_length
