cf9b46f7174f0d82d551875355c62610d639f9c0
Zeamanual Feleke <91523382+zeamanual@users.noreply.github.com>
2023-07-30 21:30:45 +0300

    Create Merging Arrays.py

---
+++ Merging Arrays/Merging Arrays.py
@@ -0,0 +1,25 @@
+n,m = list(map(int,input().split()))
+nums1=list(map(int,input().split()))
+nums2=list(map(int,input().split()))
+
+ptr1=0
+ptr2=0
+ 
+sorted_arr=[]
+while(ptr1<n and ptr2 <m):
+    if(nums1[ptr1]>nums2[ptr2]):
+        sorted_arr.append(nums2[ptr2])
+        ptr2+=1
+    else:
+        sorted_arr.append(nums1[ptr1])
+        ptr1+=1
+ 
+while(ptr1<n):
+    sorted_arr.append(nums1[ptr1])
+    ptr1+=1
+ 
+while(ptr2<m):
+    sorted_arr.append(nums2[ptr2])
+    ptr2+=1
+ 
+print(*sorted_arr)
