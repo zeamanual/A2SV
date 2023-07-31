41ab941a46dfdc5346ecd62defa5c02d4de9f420
Zeamanual Feleke <91523382+zeamanual@users.noreply.github.com>
2023-07-31 19:31:58 +0300

    Create 1646. Get Maximum in Generated Array.py

---
+++ 1646. Get Maximum in Generated Array/1646. Get Maximum in Generated Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        
+        if n < 2:
+            return n
+        arr = [0]*(n+1)
+        arr[1] = 1
+        max_num = 0
+        for i in range(2, n+1):
+            if i%2 == 0:
+                arr[i] = arr[i//2]
+            else:
+                arr[i] = arr[i//2] + arr[i//2 + 1]
+            max_num = max(max_num, arr[i])
+        return max_num
