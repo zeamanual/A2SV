2c46171fd29ae66038f4ccfcccca138bf51f7a27
Zeamanual Feleke <91523382+zeamanual@users.noreply.github.com>
2023-09-18 10:55:51 +0300

    650. 2 Keys Keyboard.py

--- Testcase Testcase Code Code Result  650. 2 Keys Keyboard/Testcase Testcase Code Code Result  650. 2 Keys Keyboard.py
+++ Testcase Testcase Code Code Result  650. 2 Keys Keyboard/Testcase Testcase Code Code Result  650. 2 Keys Keyboard.py
@@ -2,7 +2,7 @@ class Solution:
     def minSteps(self, n: int) -> int:
 
         @lru_cache(None)
-        def helper(count,clipboard):
+        def helper(count,clipboard,copy_flag):
 
             print(count,clipboard)
             if(count>n or clipboard>n):
@@ -10,8 +10,8 @@ class Solution:
             elif(count==n):
                 return 0
 
-            coping_result = 2 + helper(count*2,count)
-            pasting_result = 1+ helper(count+clipboard,clipboard) if clipboard else float('inf')
+            coping_result = 1 + helper(count,count,True) if not copy_flag else float('inf') 
+            pasting_result = 1 + helper(count+clipboard,clipboard,False) if clipboard else float('inf')
             return min(coping_result,pasting_result)
 
-        return helper(1,0)
+        return helper(1,0,False)
